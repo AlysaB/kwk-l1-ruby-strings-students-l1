d654f0c2cf4e1e1f5f9e0e781474625353598547
Alysa Barbarisi <baal02@cox.net>
2018-06-05 22:08:23 +0000

    automatically backed up by learn

---
+++ test.rb
@@ -0,0 +1,49 @@
+#age = 20
+#if age <10 
+  #puts "You are too young. BYE!"
+#end
+
+  #if age >10
+  #  puts "Welcome!"
+  #end
+  
+  
+#age = 18
+#if age >18
+  #puts "Welcome!"
+#else
+#  puts "Too young"
+#end
+
+#grade = 80
+#if grade >70
+ #puts "Passed!"
+#else
+  #puts "Failed!"
+#end 
+
+#a = gets.chomp.to_i 
+#if a>10
+ # puts "Your number is greater than 10"
+#else
+#  puts "Your number is not greater than 10"
+#end 
+
+
+
+#if Time.now.hour >= 7 && Time.now.hour <=11
+#  puts "Good Morning!"
+#elsif Time.now.hour >= 11 && Time.now.hour <=17
+#  puts "Good Afternoon"
+#elsif Time.now.hour >= 17 && Time.now.hour <=20
+ # puts "Good Evening"
+#else 
+#  puts "Good Night"
+#end
+
+
+
+
+
+
+
